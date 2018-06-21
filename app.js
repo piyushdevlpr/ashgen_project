7fb5855fcfa8998048ea744b49c45c4a4428a677
piyushdevlpr <piyush80108@gmail.com>
2018-06-21 10:16:25 +0530

    login&signup

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 app.get("/",function(req,res){
  // res.sendFile(path.join(__dirname+'/login.html'));	
 	res.render("login.ejs");
@@ -16,3 +16,5 @@ app.get("/signin",function(req,res){
 app.listen(port,function(){
 	console.log("server running!!!!");
 });
+
+// https://sheltered-island-82789.herokuapp.com/
