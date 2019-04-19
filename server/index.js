9b52e97133e31875e078a0bd59fde508e6a535b7
mkgdev <mukeshinhere07@gmail.com>
2019-04-19 18:01:46 +0530

    get user

--- server/index.js
+++ server/index.js
@@ -21,7 +21,8 @@ var express     = require("express"),
     multer = require("multer"),
     path = require("path"),
     server= require("http").createServer(app),
-    io = require("socket.io").listen(server) 
+    io = require("socket.io").listen(server),
+    getUserRoute = require('./routes/getUser'); 
 
 
 
@@ -66,6 +67,7 @@ passport.deserializeUser(Team.deserializeUser());
 //   next();
 // });
 var port = process.env.PORT || 2000;
+app.use(getUserRoute);
 // app.use(function(req, res, next){
 //    res.locals.currentUser = req.user;
 //    next();
